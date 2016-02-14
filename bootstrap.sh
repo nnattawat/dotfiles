60adfe192b878bb31ead0c89340f4a06055b9de4
nnattawat <armmer1@gmail.com>
2016-02-14 11:42:16 +1100

    Changed files: bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y apache2
+apt-get install -y apache2 nodejs npm nodejs-legacy
 if ! [ -L /var/www  ]; then
   rm -rf /var/www
   mkdir /var/www
